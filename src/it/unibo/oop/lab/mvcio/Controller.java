7d5a94d5b0792f73c482be5d8421f0c91b641a03
Gianluca Ensini <gianluca.ensini@studio.unibo.it>
2021-04-15 23:54:10 +0200

    mvcio

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,9 +1,23 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * 
  */
 public class Controller {
+
+    private static final String USER_HOME = System.getProperty("user.home");
+    private static final String SEP = System.getProperty("file.separator");
+    private static final String DEF_NAME = "output.txt";
+
+    private File currentFile;
+
+    public Controller() {
+        setDefault();
+    }
 
     /*
      * This class must implement a simple controller responsible of I/O access. It
@@ -12,14 +26,60 @@ public class Controller {
      * Implement this class with:
      * 
      * 1) A method for setting a File as current file
+     */
+
+    /**
      * 
+     * @param file
+     *                 .
+     */
+    public void setFile(final File file) {
+        this.currentFile = file;
+    }
+
+    /*
      * 2) A method for getting the current File
+     */
+
+    /**
      * 
+     * @return .
+     */
+    public File getFile() {
+        return this.currentFile;
+    }
+
+    /*
      * 3) A method for getting the path (in form of String) of the current File
+     */
+
+    /**
      * 
+     * @return .
+     */
+    public String getPath() {
+        return this.currentFile.getPath();
+    }
+
+    /*
      * 4) A method that gets a String as input and saves its content on the current
      * file. This method may throw an IOException.
+     */
+
+    /**
      * 
+     * @param text
+     *                 .
+     * @throws IOException
+     *                         .
+     */
+    public void addText(final String text) throws IOException {
+        try (PrintStream ps = new PrintStream(currentFile)) {
+            ps.print(text);
+        }
+    }
+
+    /*
      * 5) By default, the current file is "output.txt" inside the user home folder.
      * A String representing the local user home folder can be accessed using
      * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
@@ -27,5 +87,9 @@ public class Controller {
      * System.getProperty("file.separator"). The combined use of those methods leads
      * to a software that runs correctly on every platform.
      */
+
+    private void setDefault() {
+        this.currentFile = new File(USER_HOME + SEP + DEF_NAME);
+    }
 
 }
--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,14 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -17,9 +22,13 @@ public final class SimpleGUI {
      * Once the Controller is done, implement this class in such a way that:
      * 
      * 1) It has a main method that starts the graphical application
-     * 
+     */
+
+    /*
      * 2) In its constructor, sets up the whole view
-     * 
+     */
+
+    /*
      * 3) The graphical interface consists of a JTextArea with a button "Save" right
      * below (see "ex02.png" for the expected result). SUGGESTION: Use a JPanel with
      * BorderLayout
@@ -33,30 +42,53 @@ public final class SimpleGUI {
      * Use "ex02.png" (in the res directory) to verify the expected aspect.
      */
 
+    public static void main(final String[] args) {
+        new SimpleGUI();
+    }
+
     /**
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
         /*
-         * Make the frame half the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected.
+         * Make the frame half the resolution of the screen. This very method is enough
+         * for a single screen setup. In case of multiple monitors, the primary is
+         * selected.
          * 
-         * In order to deal coherently with multimonitor setups, other
-         * facilities exist (see the Java documentation about this issue). It is
-         * MUCH better than manually specify the size of a window in pixel: it
-         * takes into account the current resolution.
+         * In order to deal coherently with multimonitor setups, other facilities exist
+         * (see the Java documentation about this issue). It is MUCH better than
+         * manually specify the size of a window in pixel: it takes into account the
+         * current resolution.
          */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / 2, sh / 2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel panel = new JPanel(new BorderLayout());
+        frame.setContentPane(panel);
+        final JButton saveButton = new JButton("read");
+        final JTextArea textArea = new JTextArea();
+        panel.add(saveButton, BorderLayout.CENTER);
+        panel.add(textArea, BorderLayout.NORTH);
+
+        saveButton.addActionListener((e) -> {
+
+            if (JOptionPane.showOptionDialog(frame, "che vogliamo fà", "questa è una finestruccia", JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null, null, null) == JOptionPane.OK_OPTION) {
+
+            }
+
+        });
         /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
